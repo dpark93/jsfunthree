465c4bd11d22c5159b9d7fb86675d43b1319134d
GogogoDavis <burkedavis@yahoo.com>
2023-10-03 12:26:41 -0600

    Updates

--- index.js
+++ index.js
@@ -14,7 +14,41 @@ var listItems = document.querySelector("#myList");
 var newElement = document.createElement('li');
 var newContent = document.createTextNode(userInput);
 newElement.appendChild(newContent);
-listItems.appendChild(newElement)
+var span = document.createElement("SPAN");
+  var txt = document.createElement("button");
+  span.className = "close";
+  span.appendChild(txt);
+listItems.appendChild(newElement);
+listItems.appendChild(span); 
+
+
+// var myNodelist = document.getElementsByTagName("li");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createElement("button");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist.appendChild(span);
+// }
+ }
+)
+
+txt.addEventListener("click", () => {
+    var a = document.getElementById("myList");
+    var close = document.getElementsByClassName("txt")
+    var item = document.getElementById(txt.value);
+    a.removeChild(item);
+    //close.closest('li').remove(); 
 })
+// (span).closest('li').remove()
 
-
+// var myNodelist = document.getElementsByTagName("li");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("button");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
