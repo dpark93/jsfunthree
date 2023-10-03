16ff3e14f9f3cdac45380d9fe2e6e0fa64c3a745
dpark93 <d.park.official@gmail.com>
2023-10-03 10:12:23 -0600

    able to add to the list now

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let btn = document.querySelector("#summitBtn");
-let inputField = document.getElementById("mainInput");
-let userInput = "";
-let listItems = document.querySelector("#myList");
+var btn = document.querySelector("#summitBtn");
+var inputField = document.getElementById("mainInput");
+var userInput = "";
+// let listItems = document.querySelector("#myList");
 // let newElement = document.createElement('li');
 // let newContent = document.createTextNode(userInput);
-
+// newElement.appendChild(newContent);
 
 
 inputField.addEventListener("input", (obj) => {userInput = obj.target.value})
 
 btn.addEventListener("click", () => {
-    let newElement = document.createElement('li');
-    let newContent = document.createTextNode(userInput);
-    listItems.appendChild(newContent);
-    +"/n"
+var listItems = document.querySelector("#myList");
+var newElement = document.createElement('li');
+var newContent = document.createTextNode(userInput);
+newElement.appendChild(newContent);
+listItems.appendChild(newElement)
 })
 
 
