0c43ed60f189db5b03ea2b998d90d0c73b71b961
dpark93 <d.park.official@gmail.com>
2023-10-03 13:25:07 -0600

    add and delete function working

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-var btn = document.querySelector("#summitBtn");
-var inputField = document.getElementById("mainInput");
-var userInput = "";
-// let listItems = document.querySelector("#myList");
-// let newElement = document.createElement('li');
-// let newContent = document.createTextNode(userInput);
-// newElement.appendChild(newContent);
+// var btn = document.querySelector("#summitBtn");
+// var inputField = document.getElementById("mainInput");
+// var userInput = "";
+// inputField.addEventListener("input", (obj) => {userInput = obj.target.value})
 
 
-inputField.addEventListener("input", (obj) => {userInput = obj.target.value})
 
-btn.addEventListener("click", () => {
-var listItems = document.querySelector("#myList");
-var newElement = document.createElement('li');
-var newContent = document.createTextNode(userInput);
-newElement.appendChild(newContent);
-var span = document.createElement("SPAN");
-  var txt = document.createElement("button");
-  span.className = "close";
-  span.appendChild(txt);
-listItems.appendChild(newElement);
-listItems.appendChild(span); 
+// function addNewElement() {
+//     var listItems = document.querySelector("#myList");
+//     var newElement = document.createElement('li');
+//     var newContent = document.createTextNode(userInput);
+//     newElement.appendChild(newContent);
+//     listItems.appendChild(newElement);
 
+//     var span = document.createElement("SPAN")    
+//     var txt = document.createElement("button");
+//     span.className = "close";
+    
+//     span.appendChild(txt);
+//     listItems.appendChild(span); 
 
-// var myNodelist = document.getElementsByTagName("li");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createElement("button");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist.appendChild(span);
+//      var close = document.getElementsByClassName("close");
+//      for (i = 0; i < close.length; i++) {
+//      close[i].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
 // }
- }
-)
 
-txt.addEventListener("click", () => {
-    var a = document.getElementById("myList");
-    var close = document.getElementsByClassName("txt")
-    var item = document.getElementById(txt.value);
-    a.removeChild(item);
-    //close.closest('li').remove(); 
-})
-// (span).closest('li').remove()
 
-// var myNodelist = document.getElementsByTagName("li");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("button");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("mainInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById("myList").appendChild(li);
+  
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u2613");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+  
+  var close = document.getElementsByClassName("close");
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+
